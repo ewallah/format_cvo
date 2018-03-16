6c8c30df6075605742a9fbc3aa2e7b266d93f748
eWallah <info@eWallah.net>
2018-03-16 12:49:12 +0100

    local privacy

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -27,7 +27,7 @@ namespace format_cvo\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-class provider implements \core_privacy\metadata\null_provider {
+class provider implements \core_privacy\local\metadata\null_provider {
 
     /**
      * Get the language string identifier with the component's language
